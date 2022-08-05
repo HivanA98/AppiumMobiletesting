166d1d072a57258862b569fbf215fb02505f30f3
HIvan <hivanarmadi@gmail.com>
2022-08-05 20:23:11 +0700

    menyesuaikan

--- test/specs/Test 1 Appium.js
+++ test/specs/Test 1 Appium.js
@@ -3,7 +3,7 @@ describe('First testing use appium with webdriver', () => {
     it('Should succesfully fill the form', async () => {
         
         await $('~Login').Forms
-        await expect($('~Login-screen')).toBeDisplayed();
+        //await expect($('~Login-screen')).toBeDisplayed();
 
         await $('~text-input').click.setValue('Hai There, i`am Ivan')
         await expect($('~input-text-result')).toHaveValue('Hai There, i`am Ivan');
@@ -15,7 +15,7 @@ describe('First testing use appium with webdriver', () => {
         await $('/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/androidx.appcompat.widget.LinearLayoutCompat/android.widget.FrameLayout/android.widget.ListView/android.widget.CheckedTextView[2]').click
         await expect($('~00000000-0000-0032-ffff-ffff00000059')).toHaveValue('webdriver.io is awesome');
 
-        await $('~00000000-0000-0032-ffff-ffff00000064')
+        //await $('~00000000-0000-0032-ffff-ffff00000064')
         await expect($('~00000000-0000-0032-ffff-ffff00000063')).toHaveValue('Clickable=false');
     });
 
